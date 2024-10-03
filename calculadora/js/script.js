070fa249578e7218d2a4572cc2ca1ce4e00a997c
GabrielDaZK <gabriel.antunes.correa@escola.pr.gov.br>
2024-10-03 15:21:59 +0000

    inciado o codigo javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,15 @@
+// faz referencia aos elementos Dom
+const numero1 = document.querySelector('numero1')
+const numero2 = document.querySelector('numero1')
+const btnCalcular = document.querySelector('#btn-calcular')
+//alert(numero1)
+const resultado = document.querySelector('.resultado')
+const adicao = document.querySelector('.adicao')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('multiplicacao')
+const divisao = document.querySelector('.divisao')
+
+// cria a função calcular
+
+
+// adociona executar de eventos
